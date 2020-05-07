b69294440390af4db4408fef8c2f77c38bb80933
ramonskie <ramonmakkelie@gmail.com>
2020-05-07 12:23:17 +0200

    creating spec files for all features

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -15,17 +15,92 @@ var _ = Describe("Interal Kit", func() {
 	})
 
 	Test(Environment{
-		Focus:       true,
-		Name:        "baseline",
+		Name:        "bare",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	// ROUTING
+	Test(Environment{
+		Name:        "haproxy",
 		CloudConfig: "aws",
 		CPI:         "aws",
 	})
 	Test(Environment{
-		Name:        "openvpn",
+		Name:        "haproxy-tls",
 		CloudConfig: "aws",
+		CPI:         "aws",
 	})
 	Test(Environment{
-		Name:        "provided-user",
+		Name:        "haproxy-self-signed",
 		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	// DB
+	Test(Environment{
+		Name:        "mysql-db",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "postgres-db",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	// BLOBSTORE
+	Test(Environment{
+		Name:        "blobstore-aws",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "blobstore-gcp",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "blobstore-azure",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	// FEATURES
+	Test(Environment{
+		Name:        "container-routing-integrity",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "native-garden-runc",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "loggregator-forwarder-agent",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "dns-service-discovery",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "routing-api",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "app-autoscaler-integration",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "small-footprint",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
+	Test(Environment{
+		Name:        "nfs-volume-services",
+		CloudConfig: "aws",
+		CPI:         "aws",
 	})
 })
