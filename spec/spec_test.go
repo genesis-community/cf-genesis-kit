228a2e458477fc955ff2699cea11a8a0c98bcfdc
rkoster <rkoster@starkandwayne.com>
2021-02-18 09:08:48 +0100

    Extract db-override-name related bosh_variables into own migration file

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -160,7 +160,6 @@ var _ = Describe("Interal Kit", func() {
 		RuntimeConfig: "dns",
 		CPI:           "aws",
 		Exodus:        "v1",
-		Focus:         true,
 	})
 	Test(Environment{
 		Name:          "upgraded-from-v1",
@@ -168,8 +167,14 @@ var _ = Describe("Interal Kit", func() {
 		RuntimeConfig: "dns",
 		CPI:           "aws",
 		Exodus:        "migrated",
-		Focus:         true,
 	})
+	Test(Environment{
+		Name:          "upgrade-from-v1-with-db-override-names",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+		Exodus:        "migrated",
+	})	
 	// Test(Environment{
 	// 	Focus:       true,
 	// 	Name:        "nfs-volume-services",
