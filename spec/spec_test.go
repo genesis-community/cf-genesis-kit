4caeb97b8503663dd260dc582b5b1b8f34db84dc
Patryk Kozak <thehackbee@gmail.com>
2022-06-24 13:47:57 +0200

    OCF-221: Restore test suite

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -14,209 +14,209 @@ var _ = Describe("Interal Kit", func() {
 		KitDir, _ = filepath.Abs(filepath.Join(filepath.Dir(filename), "../"))
 	})
 
-	// Test(Environment{
-	// 	Name:          "bare",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// // ROUTING
-	// Test(Environment{
-	// 	Name:          "haproxy",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "haproxy-tls",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CredhubVars:   "haproxy-tls",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "haproxy-self-signed",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// // DB
-	// Test(Environment{
-	// 	Name:          "mysql-db",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "postgres-db",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// // BLOBSTORE
-	// Test(Environment{
-	// 	Name:          "blobstore-aws",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	// CredhubVars:   "aws",
-	// 	CPI: "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "blobstore-gcp",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CredhubVars:   "gcp",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "blobstore-azure",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CredhubVars:   "azure",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "blobstore-minio",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// // FEATURES
-	// Test(Environment{
-	// 	Name:          "container-routing-integrity",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "native-garden-runc",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "loggregator-forwarder-agent",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "dns-service-discovery",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "routing-api",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "app-autoscaler-integration",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "small-footprint",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "availability-zones",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "no-tcp-routers",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "router-synergy",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "override-vm-types-and-counts",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "override-vm-types-and-counts-old-names",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "upgrading-to-v2",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// 	Exodus:        "v1",
-	// })
-	// Test(Environment{
-	// 	Name:          "upgraded-from-v1",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// 	Exodus:        "migrated",
-	// })
-	// Test(Environment{
-	// 	Name:          "upgrade-from-v1-with-db-override-names",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// 	Exodus:        "migrated",
-	// })
-	// Test(Environment{
-	// 	Name:          "upgrading-to-v2-with-204-overrides",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// 	Exodus:        "v1",
-	// })
-	// Test(Environment{
-	// 	Name:          "upgraded-from-v1-with-204-overrides",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// 	Exodus:        "migrated",
-	// })
-	// Test(Environment{
-	// 	Name:          "compiled-releases",
-	// 	CloudConfig:   "aws",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws",
-	// })
-	// Test(Environment{
-	// 	Name:          "compiled-windows-releases",
-	// 	CloudConfig:   "aws", //"azure",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws", //"azure",
-	// })
-	// Test(Environment{
-	// 	Name:          "windows-and-smb-support",
-	// 	CloudConfig:   "aws", //"gcp",
-	// 	RuntimeConfig: "dns",
-	// 	CPI:           "aws", //"gcp",
-	// })
+	Test(Environment{
+		Name:          "bare",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	// ROUTING
+	Test(Environment{
+		Name:          "haproxy",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "haproxy-tls",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CredhubVars:   "haproxy-tls",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "haproxy-self-signed",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	// DB
+	Test(Environment{
+		Name:          "mysql-db",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "postgres-db",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	// BLOBSTORE
+	Test(Environment{
+		Name:          "blobstore-aws",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		// CredhubVars:   "aws",
+		CPI: "aws",
+	})
+	Test(Environment{
+		Name:          "blobstore-gcp",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CredhubVars:   "gcp",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "blobstore-azure",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CredhubVars:   "azure",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "blobstore-minio",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	// FEATURES
+	Test(Environment{
+		Name:          "container-routing-integrity",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "native-garden-runc",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "loggregator-forwarder-agent",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "dns-service-discovery",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "routing-api",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "app-autoscaler-integration",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "small-footprint",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "availability-zones",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "no-tcp-routers",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "router-synergy",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "override-vm-types-and-counts",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "override-vm-types-and-counts-old-names",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "upgrading-to-v2",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+		Exodus:        "v1",
+	})
+	Test(Environment{
+		Name:          "upgraded-from-v1",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+		Exodus:        "migrated",
+	})
+	Test(Environment{
+		Name:          "upgrade-from-v1-with-db-override-names",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+		Exodus:        "migrated",
+	})
+	Test(Environment{
+		Name:          "upgrading-to-v2-with-204-overrides",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+		Exodus:        "v1",
+	})
+	Test(Environment{
+		Name:          "upgraded-from-v1-with-204-overrides",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+		Exodus:        "migrated",
+	})
+	Test(Environment{
+		Name:          "compiled-releases",
+		CloudConfig:   "aws",
+		RuntimeConfig: "dns",
+		CPI:           "aws",
+	})
+	Test(Environment{
+		Name:          "compiled-windows-releases",
+		CloudConfig:   "aws", //"azure",
+		RuntimeConfig: "dns",
+		CPI:           "aws", //"azure",
+	})
+	Test(Environment{
+		Name:          "windows-and-smb-support",
+		CloudConfig:   "aws", //"gcp",
+		RuntimeConfig: "dns",
+		CPI:           "aws", //"gcp",
+	})
 	Test(Environment{
 		Name:          "isolation-segments",
 		CloudConfig:   "aws",
 		RuntimeConfig: "dns",
 		CPI:           "aws",
 	})
-	// Test(Environment{
-	// 	Focus:       true,
-	// 	Name:        "nfs-volume-services",
-	// 	CloudConfig: "aws",
-	// 	CPI:         "aws",
-	// })
+	Test(Environment{
+		Focus:       true,
+		Name:        "nfs-volume-services",
+		CloudConfig: "aws",
+		CPI:         "aws",
+	})
 })
