164575dccaec2c18a54cf2db9bf4a4d639910b6e
Norman Abramovitz <norman-abramovitz@users.noreply.github.com>
2024-06-25 16:14:29 -0700

    Ginkgo V2 and dependencies upgrade (#227)
    * changes for ginkgo v2 conversion
    
    * update dependencies based upon develop branch
    
    * upgraded testkit to v2
    
    * upgrade testkit to work with genesis v2 or v3

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -4,15 +4,16 @@ import (
 	"path/filepath"
 	"runtime"
 
-	. "github.com/genesis-community/testkit/testing"
-	. "github.com/onsi/ginkgo"
+	. "github.com/genesis-community/testkit/v2/testing"
+	. "github.com/onsi/ginkgo/v2"
 )
 
+var _ = BeforeSuite(func() {
+	_, filename, _, _ := runtime.Caller(0)
+	KitDir, _ = filepath.Abs(filepath.Join(filepath.Dir(filename), "../"))
+})
+
 var _ = Describe("Interal Kit", func() {
-	BeforeSuite(func() {
-		_, filename, _, _ := runtime.Caller(0)
-		KitDir, _ = filepath.Abs(filepath.Join(filepath.Dir(filename), "../"))
-	})
 
 	Test(Environment{
 		Name:          "bare",
