02152ad05a9ae02f1485a591d84746cfe8803965
Kevin Rutten <krutten@fivetwenty.io>
2025-06-24 19:53:32 +0000

    Remove the + features from features.pm

--- hooks/features.pm
+++ hooks/features.pm
@@ -36,10 +36,6 @@ sub perform {
 			push @features, 'nfs-lambda';
 		} elsif ($feature eq 'cf-deployment/operations/enable-smb-volume-services') {
 			push @features, 'smb-volume-services';
-		} elsif ($feature eq 'internal-db') {
-			push @features, $is_ocfp ? '+internal-db' : 'internal-db';
-		} elsif ($feature eq 'internal-blobstore') {
-			push @features, $is_ocfp ? '+internal-blobstore' : 'internal-blobstore';
 		} elsif ($feature eq 'split-network') { # Short-lived ocfp feature that is better handled by existing feature name
 			push @features, 'partitioned-network';
 		} else {
