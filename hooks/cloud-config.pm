da312bbabf0918c3d38686a30748e58e79811969
FiveTwenty AWS Bastion <bastion@fivetwenty.io>
2025-07-23 20:34:24 +0000

    fix aws vm_type and sizes for cloud config

--- hooks/cloud-config.pm
+++ hooks/cloud-config.pm
@@ -348,22 +348,22 @@ sub _get_aws_vm_matrix {
 		map {( $_->[0], {type_dev => $_->[1], type_prod => $_->[2], disk_size => int($_->[3]), ephemeral_dev => int($_->[4]), ephemeral_prod => int($_->[5])}) } (
 			#      Name          dev_type     prod_type          root_disk(MB)  ephemeral_dev  ephemeral_prod
 			[ qw[  api           t3.medium    m6i.xlarge         15360          32768          65536  ] ],
-			[ qw[  cc-worker     t3.medium    m6i.large          15360           4096           8192  ] ],
+			[ qw[  cc-worker     t3.medium    m6i.large          15360           4096          16384  ] ],
 			[ qw[  credhub       t3.medium    r6i.large          30720           4096          16384  ] ],
 			[ qw[  diego-api     t3.large     c6i.2xlarge        15360           4096          16384  ] ],
 			[ qw[  diego-cell    t3.large     r6i.2xlarge       262144          65536         393216  ] ],
 			[ qw[  doppler       t3.medium    c6i.xlarge         15360           4096          16384  ] ],
 			[ qw[  errand        t3.medium    m6i.large          15360           4096           8192  ] ],
 			[ qw[  log-api       t3.medium    c6i.xlarge         15360           8192          16384  ] ],
-			[ qw[  log-cache     t3.large     r6i.2xlarge        15360           8192          16384  ] ],
+			[ qw[  log-cache     t3.large     r6i.2xlarge        15360           8192          32768  ] ],
 			[ qw[  nats          t3.medium    m6i.large          15360           8192          16384  ] ],
 			[ qw[  router        t3.medium    c6i.xlarge         15360           4096          16384  ] ],
-			[ qw[  scheduler     t3.medium    m6i.large          15360           4096           8192  ] ],
+			[ qw[  scheduler     t3.medium    m6i.large          15360           4096          16384  ] ],
 			[ qw[  tcp-router    t3.medium    c6i.xlarge         10240           4096          16384  ] ],
 			[ qw[  uaa           t3.large     c6i.large          30720           4096          16384  ] ],
 			[ qw[  database      t3.medium    m6i.xlarge         61440           4096          16384  ] ],
 			[ qw[  blobstore     t3.medium    m6i.large          61440           4096           8192  ] ],
-			[ qw[  windows-cell  t3.medium    r6i.2xlarge       262144          65536         393216  ] ],
+			[ qw[  windows-cell  t3.large    r6i.2xlarge       262144          65536         393216  ] ],
 		)
 	}
 }
