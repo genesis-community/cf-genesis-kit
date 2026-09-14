b19eccb50d270a72250ada1fd24c22cbe7d3c45b
Wayne E. Seguin <wayneeseguin@gmail.com>
2026-09-14 17:05:28 -0400

    Pass a single options hash to every run() in the stratos addon
    Genesis::run shifts at most one leading hashref off its argument list and
    treats the next value as the program to execute. Sixteen call sites in the
    stratos addon passed two hashrefs, so the second one was stringified as the
    command and every shell-out in the deploy path and in the security-group
    helper failed with a bash syntax error naming a HASH address.
    
    Merge each pair into the single hash that run() actually reads.

--- hooks/addon-stratos~st.pm
+++ hooks/addon-stratos~st.pm
@@ -438,19 +438,18 @@ sub deploy_stratos {
 	if ( $options{file} && -f $options{file} ) {
 		info( "Using provided Stratos file: %s", $options{file} );
 		run(
-			{interactive => 0},
-			{ onfailure => "Failed to unzip Stratos file" }, 'unzip', '-o', $options{file}
+			{ interactive => 0, onfailure => "Failed to unzip Stratos file" },
+			'unzip', '-o', $options{file}
 		);
 	}
 	else {
 		info( "Downloading Stratos %s...", $stratos_version );
 		run(
-			{interactive => 0},
-			{ onfailure => "Failed to download Stratos" }, 'wget', $stratos_releases_url
+			{ interactive => 0, onfailure => "Failed to download Stratos" },
+			'wget', $stratos_releases_url
 		);
 		run(
-			{interactive => 0},
-			{ onfailure => "Failed to unzip Stratos" },
+			{ interactive => 0, onfailure => "Failed to unzip Stratos" },
 			'unzip', '-o', "stratos-ui-${stratos_version}.zip"
 		);
 		unlink("stratos-ui-${stratos_version}.zip");
@@ -459,13 +458,11 @@ sub deploy_stratos {
 	# Target the correct CF organization and space
 	info("Targeting CF organization 'system' and space 'stratos'...");
 	run(
-		{interactive => 0},
-		{ onfailure => "Failed to create space" },
+		{ interactive => 0, onfailure => "Failed to create space" },
 		'cf', 'create-space', '-o', 'system', 'stratos'
 	);
 	run(
-		{interactive => 0},
-		{ onfailure => "Failed to target space" },
+		{ interactive => 0, onfailure => "Failed to target space" },
 		'cf', 'target', '-o', 'system', '-s', 'stratos'
 	);
 
@@ -480,24 +477,23 @@ sub deploy_stratos {
 	# Check if service already exists
 	my ( $org_guid, $org_rc ) =
 	  run(
-			{interactive => 0},
-			{ stderr => 0 }, 'cf', 'org', 'system', '--guid'
+			{ interactive => 0, stderr => 0 },
+			'cf', 'org', 'system', '--guid'
 		);
 	bail("Failed to get organization GUID") if $org_rc != 0;
 	chomp($org_guid);
 
 	my ( $space_guid, $space_rc ) =
 	  run(
-			{interactive => 0},
-			{ stderr => 0 }, 'cf', 'space', 'stratos', '--guid'
+			{ interactive => 0, stderr => 0 },
+			'cf', 'space', 'stratos', '--guid'
 		);
 	bail("Failed to get space GUID") if $space_rc != 0;
 	chomp($space_guid);
 
 	# Check if service exists using CF API
 	my ( $svc_list, $svc_rc ) = run(
-		{interactive => 0},
-		{ stderr => 0 },
+		{ interactive => 0, stderr => 0 },
 		'cf', 'curl',
 		"/v3/service_instances?organization_guids=${org_guid}&space_guids=${space_guid}"
 	);
@@ -507,8 +503,8 @@ sub deploy_stratos {
 	my $svc_exists = '';
 	my ( $jq_out, $jq_rc ) =
 	  run(
-			{interactive => 0},
-			{ stderr => 0 }, 'jq', '-r', ".resources[]|select(.name|test(\"${svc_name}\"))|.name"
+			{ interactive => 0, stderr => 0 },
+			'jq', '-r', ".resources[]|select(.name|test(\"${svc_name}\"))|.name"
 		);
 	if ( $jq_rc == 0 ) {
 
@@ -520,8 +516,7 @@ sub deploy_stratos {
 		close $svc_fh;
 
 		( $svc_exists, $jq_rc ) = run(
-			{interactive => 0},
-			{ stderr => 0 },
+			{ interactive => 0, stderr => 0 },
 			'jq', '-r', ".resources[]|select(.name|test(\"${svc_name}\"))|.name",
 			$temp_svc_file
 		);
@@ -545,8 +540,8 @@ EOF
 
 	my ( $db_json, $rc_db ) =
 	  run(
-			{interactive => 0},
-			{ stderr => 0 }, 'spruce', 'json', "$tmp_dir/db.yml"
+			{ interactive => 0, stderr => 0 },
+			'spruce', 'json', "$tmp_dir/db.yml"
 		);
 	bail("Failed to convert database config to JSON") if $rc_db != 0;
 	chomp($db_json);
@@ -562,16 +557,14 @@ EOF
 	if ( $svc_exists eq $svc_name ) {
 		info( "Service %s was found, updating existing cups service definition.", $svc_name );
 		run(
-			{interactive => 0},
-			{ onfailure => "Failed to update service" },
+			{ interactive => 0, onfailure => "Failed to update service" },
 			'cf', 'uups', $svc_name, '-p', $db_json_file
 		);
 	}
 	else {
 		info( "Service %s was not found, creating cups service definition.", $svc_name );
 		run(
-			{interactive => 0},
-			{ onfailure => "Failed to create service" },
+			{ interactive => 0, onfailure => "Failed to create service" },
 			'cf', 'cups', $svc_name, '-p', $db_json_file
 		);
 	}
@@ -676,22 +669,19 @@ sub _ensure_app_ssh_security_group {
 
 	if ( run( {interactive => 0, passfail => 1, stderr => 0 }, 'cf', 'security-group', $sg_name ) ) {
 		run(
-			{interactive => 0},
-			{ onfailure => "Failed to update security group $sg_name" },
+			{ interactive => 0, onfailure => "Failed to update security group $sg_name" },
 			'cf', 'update-security-group', $sg_name, $sg_file
 		);
 	}
 	else {
 		run(
-			{interactive => 0},
-			{ onfailure => "Failed to create security group $sg_name" },
+			{ interactive => 0, onfailure => "Failed to create security group $sg_name" },
 			'cf', 'create-security-group', $sg_name, $sg_file
 		);
 	}
 
 	run(
-		{interactive => 0},
-		{ onfailure => "Failed to bind security group $sg_name" },
+		{ interactive => 0, onfailure => "Failed to bind security group $sg_name" },
 		'cf', 'bind-security-group', $sg_name, 'system',
 		'--space', 'stratos', '--lifecycle', 'running'
 	);
